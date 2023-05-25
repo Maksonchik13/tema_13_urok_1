9735b2a9cb690bab5c862f2307ce4fd21edfb122
Пользователь <@-PC>
2023-05-25 16:02:51 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Random random = new Random();
+Console.WriteLine("Компьютер загадал число от 1 до 5");
+bool error = false;
+int rand = random.Next(1, 6);
+for (int i = 1; i <=3; i++)
+{
+    Console.Write("Введите число ");
+    int n = int.Parse(Console.ReadLine());
+    if (rand == n)
+    {
+        Console.WriteLine("Вы отгадали!");
+        break;
+    }
+    else
+    {
+        if (i<3)
+        Console.WriteLine("Неправильно, попробуйте еще раз!");
+        else
+        error = true;
+    }
+}
+if (error)
+    Console.WriteLine("Вы проиграли! Загаданное число:"+rand);
